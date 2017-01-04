2503712a9fbc313c7eb0938dbd3eb5cee1c81693
ebw8995 <emmaweeks@Emmas-MacBook-Air.local>
2017-01-04 18:41:12 -0500

    Design + JS

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-var listItems = [
+var listItems = [ 
 "Buy Gifts", 
 "Watch Love Actually", 
 "Finish Harry Potter"];
 
 var list = document.getElementById("list"); // target the list id within the html
 
-for (var i = 0; i < listItems.length; i++) {
+for (var i = 0; i < listItems.length; i++) { // 'for' is a term for a loop. we pass in conditions. you use loops with arrays. 
 	var li = document.createElement("li"); // this is a reference
 	li.appendChild(document.createTextNode(listItems[i])); // accesses whatever index is witin our loop
 	list.appendChild(li);
 }
 
 function addItem() {
-	var item = document.getElementById("todo-input").value;
-	listItems.push(item);  
-	var li = document.createElement("li"); //this creates an empty list
-	li.appendChild(document.createTextNode(item)); //we can just pass in our string, we need to add in createTextNode
-	list.appendChild(li); // parent element.appendChild (child)
-	debugger;
-	document.getElementById("todo-input").value = ""; //this clears the input field so the user can keep adding
+	if (document.getElementById("todo-input").value == "") {
+		window.alert("Silly Goose! You didn't add an item to do!")
+	} else {
+	// function event.preventDefault(); //why won't this prevent my bullet from appearing?
+		var item = document.getElementById("todo-input").value; //var is a semantic container for something, can hold a function, a number, a string, an array. You access it by typing it, no ""
+		listItems.push(item);  //push is something you can only to an array. listItems is the array (list) at the top. Pushing (item) to that array adds the next item to the list
+		var li = document.createElement("li"); //this creates an empty list
+		li.appendChild(document.createTextNode(item)); //we cant just pass in our string, we need to add in createTextNode
+		list.appendChild(li); // parent element.appendChild (child)
+		// debugger;
+		document.getElementById("todo-input").value = ""; //this clears the input field so the user can keep adding
+	}
 }
 
 
